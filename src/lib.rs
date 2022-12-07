73d13b9fdbcbaf7360459b4108526454aa1f7d87
akeamc <ake@amcoff.net>
2022-12-07 21:56:32 +0100

    spaghetti

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use std::{collections::HashMap, fs::File, io::BufReader, path::Path};
 
 use geo::Point;
-use osmpbfreader::{Node, NodeId, OsmObj, OsmPbfReader, Relation, RelationId, Way, WayId};
+use osmpbfreader::{Node, NodeId, OsmId, OsmObj, OsmPbfReader, Relation, RelationId, Way, WayId};
 
 #[derive(Debug, Default)]
 pub struct Planet {
@@ -29,31 +29,50 @@ impl Planet {
         }
     }
 
+    pub fn obj_coords(&self, obj: &OsmId) -> Option<Point> {
+        match obj {
+            OsmId::Node(n) => self.node_coords(n),
+            OsmId::Way(w) => self.node_coords(self.ways.get(w)?.nodes.first()?),
+            OsmId::Relation(_) => todo!(),
+        }
+    }
+
     pub fn node_coords(&self, node: &NodeId) -> Option<Point> {
         self.nodes
             .get(node)
             .map(|node| Point::new(node.lon(), node.lat()))
     }
 
-    pub fn relation_polygon(&self, rel: &RelationId) -> Option<Vec<Vec<NodeId>>> {
+    pub fn relation_rings(&self, rel: &RelationId) -> Option<Vec<Vec<NodeId>>> {
         let rel = self.relations.get(rel)?;
 
         let mut ways: Vec<&[NodeId]> = rel
             .refs
             .iter()
-            .map(|r| {
-                let w = r.member.way()?;
-                let w = self.ways.get(&w)?;
-
-                if w.nodes.len() < 2 {
+            .filter_map(|r| match r.member {
+                osmpbfreader::OsmId::Node(_) => None,
+                osmpbfreader::OsmId::Way(w) => match r.role.as_str() {
+                    "outer" => {
+                        let w = self.ways.get(&w)?;
+                        assert!(w.nodes.len() >= 2);
+                        Some(&w.nodes[..])
+                    }
+                    "inner" => {
+                        // panic!()
+                        eprintln!("inner!!!! ({:?})", rel.id.0);
+                        None
+                    }
+                    _ => panic!(),
+                },
+                osmpbfreader::OsmId::Relation(_) => {
+                    assert_eq!(r.role, "subarea");
                     None
-                } else {
-                    Some(&w.nodes[..])
                 }
             })
-            .collect::<Option<Vec<_>>>()?;
+            .collect::<Vec<_>>();
 
         if ways.is_empty() {
+            eprintln!("EMPTY! (relation {})", rel.id.0);
             return None;
         }
 
@@ -63,17 +82,17 @@ impl Planet {
             let mut ring = ways.pop().unwrap().to_vec();
 
             while ring.first() != ring.last() {
-                let (i, way) = ways
+                let (i, w) = ways
                     .iter()
                     .enumerate()
-                    .find(|(_i, w)| ring.first() == w.last() || ring.last() == w.first())
-                    // .expect("cannot close ring!");
+                    .find(|(_i, w)| ring.last() == w.first() || ring.last() == w.last())
+                    // .unwrap();
                     ?;
 
-                if ring.first() == way.last() {
-                    ring = [way, &ring[1..]].concat();
+                if ring.last() == w.first() {
+                    ring.extend_from_slice(&w[1..]);
                 } else {
-                    ring.extend_from_slice(&way[1..]);
+                    ring.extend(w.iter().rev().skip(1));
                 }
 
                 ways.swap_remove(i);
@@ -95,7 +114,7 @@ pub fn read(path: impl AsRef<Path>) -> anyhow::Result<Planet> {
         planet.insert(res?);
     }
 
-    println!("built planet");
+    eprintln!("built planet");
 
     Ok(planet)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,115 @@
-use geojson::{Feature, FeatureCollection, GeoJson};
-use osmpbfreader::NodeId;
+use std::{collections::HashMap, fs::File};
+
+use geo::{Contains, LineString, Point, Polygon};
+use geojson::{Feature, FeatureCollection, GeoJson, PolygonType};
+use osmpbfreader::{NodeId, OsmId, OsmObj, RelationId};
+use rayon::prelude::{IntoParallelRefIterator, ParallelIterator};
+
+fn point_relations<'a>(
+    polygons: &'a [(Polygon, RelationId)],
+    point: &'a Point,
+) -> impl Iterator<Item = &'a (Polygon, RelationId)> + 'a {
+    polygons.iter().filter(|(p, _)| p.contains(point))
+}
 
 fn main() -> anyhow::Result<()> {
+    // let osm = osm::read("faroe-islands.osm.pbf")?;
+    // let osm = osm::read("albania.osm.pbf")?;
     let osm = osm::read("sweden.osm.pbf")?;
 
-    let mut rings: Vec<Vec<NodeId>> = Vec::new();
+    let polygons = osm
+        .relations
+        .values()
+        .filter_map(|rel| {
+            if rel.tags.contains("boundary", "administrative") {
+                if let Some(v) = osm.relation_rings(&rel.id) {
+                    return Some(v.into_iter().filter_map(|nodes| {
+                        let exterior = nodes
+                            .into_iter()
+                            .map(|n| osm.node_coords(&n))
+                            .collect::<Option<LineString<_>>>()?;
+                        let polygon = Polygon::new(exterior, vec![]);
+                        Some((polygon, rel.id))
+                    }));
+                } else {
+                    eprintln!("oh no!");
+                };
+            }
 
-    for rel in osm.relations.values() {
-        if rel.tags.contains("boundary", "administrative") {
-            if let Some(r) = osm.relation_polygon(&rel.id) {
-                rings.extend_from_slice(&r[..]);
-            };
+            None
+        })
+        .flatten()
+        .collect::<Vec<_>>();
+
+    eprintln!("{} polygons", polygons.len());
+
+    let buildings = osm
+        .nodes
+        .par_iter()
+        .map(|(k, v)| OsmId::Node(*k))
+        .chain(osm.ways.par_iter().map(|(k, v)| OsmId::Way(*k)));
+
+    // serde_json::to_writer(File::create("nodes.json")?, &buildings.collect::<Vec<_>>())?;
+    // serde_json::to_writer(File::create("polys.json")?, &polygons)?;
+
+    buildings.for_each(|id| {
+        let tags = match id {
+            OsmId::Node(n) => &osm.nodes.get(&n).unwrap().tags,
+            OsmId::Way(w) => &osm.ways.get(&w).unwrap().tags,
+            OsmId::Relation(_) => todo!(),
+        };
+
+        if !tags.contains_key("amenity") {
+            return;
         }
-    }
 
-    let geojson = GeoJson::FeatureCollection(FeatureCollection {
-        features: rings
+        let point = osm.obj_coords(&id).unwrap();
+        let mut rels = point_relations(&polygons, &point)
+            .map(|(_p, rel)| osm.relations.get(rel).unwrap())
+            .collect::<Vec<_>>();
+
+        rels.sort_by_key(|rel| rel.tags.get("admin_level"));
+
+        let location = rels
             .into_iter()
-            .map(|r| {
-                let geometry = geojson::Geometry::new(geojson::Value::LineString(
-                    r.iter()
-                        .map(|n| {
-                            let p = osm.node_coords(n).unwrap();
-                            vec![p.x(), p.y()]
-                        })
-                        .collect(),
-                ));
+            .rev()
+            .map(|rel| rel.tags.get("name").map(|t| t.as_str()).unwrap_or_default())
+            .collect::<Vec<_>>()
+            .join(", ");
 
-                Feature {
-                    geometry: Some(geometry),
-                    ..Default::default()
-                }
-            })
-            .collect(),
-        bbox: None,
-        foreign_members: None,
+        println!(
+            "{:?} {:?}, {}",
+            tags.get("name").map(|t| t.as_str()).unwrap_or_default(),
+            id,
+            location
+        );
     });
 
-    println!("{}", geojson);
+    // let geojson = GeoJson::FeatureCollection(FeatureCollection {
+    //     features: rings
+    //         .into_iter()
+    //         .map(|r| {
+    //             let polygon = vec![r
+    //                 .iter()
+    //                 .map(|n| {
+    //                     let p = osm.node_coords(n).unwrap();
+    //                     vec![p.x(), p.y()]
+    //                 })
+    //                 .collect()];
+
+    //             let geometry = geojson::Geometry::new(geojson::Value::Polygon(polygon));
+
+    //             Feature {
+    //                 geometry: Some(geometry),
+    //                 ..Default::default()
+    //             }
+    //         })
+    //         .collect(),
+    //     bbox: None,
+    //     foreign_members: None,
+    // });
+
+    // println!("{}", geojson);
 
     Ok(())
 }
